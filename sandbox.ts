175a7cf3af1bff2ea6adc49dfb77f66d287ce47b
Maksym Polinka <Polinka7max@gmail.com>
2020-06-01 11:44:01 +0300

    added a typescript file

--- sandbox.ts
+++ sandbox.ts
@@ -1,14 +1,21 @@
-var character = "luigi";
-var age = 30;
-var isBeltBlack = false;
+let character = "luigi";
+let age = 30;
+let isBeltBlack = false;
+
 // character = 20; //produces an error
+
 // console.log(character);
+
 // const inputs = document.querySelectorAll('input');
+
 // console.log(inputs)
+
 // inputs.forEach((input) => {
 //     console.log(input);
 // })
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(4)); //if we pass a string then we get a NaN 
+
+const circ = (diameter: number) => {
+    return diameter*Math.PI;
+}
+
+console.log(circ(4)) //if we pass a string then we get a NaN 
