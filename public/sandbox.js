9d766020961d7ccde9a9c7503d963e9f731b4363
Maksym Polinka <Polinka7max@gmail.com>
2020-06-03 10:22:11 +0300

    added function types

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("test max 1234");
+// console.log("test max 1234")
+var greet;
+greet = function () {
+    console.log("Hello, World!");
+};
+// greet = "hello" //produces an error 
+// number | string - union type
+// const add = (a: number, b: number, c: number | string = 10): void => { //by default returns void 
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 20);
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(10, 7); //result will be a number 
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,25 @@
-console.log("test max 1234")
+// console.log("test max 1234")
+
+let greet: Function;
+
+greet = () => {
+    console.log("Hello, World!")
+}
+
+// greet = "hello" //produces an error 
+// number | string - union type
+
+// const add = (a: number, b: number, c: number | string = 10): void => { //by default returns void 
+const add = (a: number, b: number, c: number | string = 10) => { //by default returns void 
+    console.log(a + b);
+    console.log(c);
+}
+
+add(5, 10, 20);
+
+const minus = (a: number, b: number): number => {
+    return a - b;
+}
+
+let result = minus(10, 7); //result will be a number 
+
